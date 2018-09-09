c359284b10aa1f8742f57503582d6cc81f82baee
Jere Suikkila <jere@suikkila.com>
2018-09-09 13:11:57 +0300

    Add generator script to create mapnik xmls

---
+++ generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+carto kandi-buildings.mml > kandi-buildings.xml
+carto kandi-landuse.mml > kandi-landuse.xml
+carto kandi-railways.mml > kandi-railways.xml
+carto kandi-roads.mml > kandi-roads.xml
+carto kandi-venues.mml > kandi-venues.xml
+carto kandi-water.mml > kandi-water.xml
